859a7db1dc8beb1d3af7e6e97e1ace20922a040d
szyszkoo <agata.szysz@gmail.com>
2018-03-25 17:40:27 +0200

    GUI added with some basic layout

--- UrbeliKuldetes/CreatingRequests/RequestCreator.cs
+++ UrbeliKuldetes/CreatingRequests/RequestCreator.cs
@@ -8,7 +8,7 @@ using RestSharp;
 
 namespace CommunicationApp.CreatingRequests
 {
-    class CreateRequest
+    class RequestCreator
     {
         public static RestRequest CreatePOSTRequest(string payload)
         {
--- UrbeliKuldetes/MainWindow.xaml.cs
+++ UrbeliKuldetes/MainWindow.xaml.cs
@@ -23,6 +23,71 @@ namespace UrbeliKuldetes
         public MainWindow ( )
         {
             InitializeComponent ( );
+            this.Title = "Space Mission";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
+        #region Commands buttons
+        private void ScanBtn_Click ( object sender, RoutedEventArgs e )
+        {
+            ParametersBox.Visibility = Visibility.Visible;
+        }
+
+        // TODO : delete this method without errors
+        private void MoveTo_Click ( object sender, RoutedEventArgs e )
+        {
+            
+        }
+        private void MoveToBtn_Click ( object sender, RoutedEventArgs e )
+        {
+            ParametersBox.Visibility = Visibility.Visible;
+        }
+
+        private void ProduceBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void HarvestBtn_Click ( object sender, RoutedEventArgs e )
+        {
+            ParametersBox.Visibility = Visibility.Visible;
+        }
+
+        private void RepairBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void OrderBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        #endregion
+        #region Parameters for Scan, MoveTo, Harvest
+        private void ChaarrBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void EsthBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void ShuttleBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void AsteroidsBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void PoludnicaBtn_Click ( object sender, RoutedEventArgs e )
+        {
+
+        }
+        #endregion
     }
 }
